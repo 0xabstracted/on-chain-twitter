99221e5a41e50adb2877f1601ead988f73bde7be
0xabstracted <vskbr23@gmail.com>
2022-05-01 04:45:52 +0530

    Adding new parameters for Tweet Accounts

--- programs/on-chain-twitter/src/lib.rs
+++ programs/on-chain-twitter/src/lib.rs
@@ -7,30 +7,38 @@ pub mod on_chain_twitter {
     use super::*;
     pub fn create_twitter_account(ctx: Context<CreateTwitterAccount>, username: String) -> Result<()> {
         let twitter_user_account = &mut ctx.accounts.twitter_user_account;
+        let clock: Clock = Clock::get().unwrap();
         if username.as_bytes().len() > 64 {
             return Err(ErrorCode::UsernameTooLong.into())
         }
         twitter_user_account.tweet_count = 0;
-        twitter_user_account.authority = *ctx.accounts.authority.key;
+        twitter_user_account.author = *ctx.accounts.author.key;
         twitter_user_account.username = username;    
         twitter_user_account.bump = *ctx.bumps.get("twitter_user_account").unwrap();
-     
+        twitter_user_account.last_interaction_timestamp = clock.unix_timestamp;
         Ok(())
     }
     pub fn change_user_name(ctx: Context<ChangeUserName>, username: String) -> Result<()>{
         let twitter_user_account = &mut ctx.accounts.twitter_user_account;
+        let clock: Clock = Clock::get().unwrap();
         if username.as_bytes().len() > 64 {
             return Err(ErrorCode::NewUsernameTooLong.into())    
         }
         twitter_user_account.username = username;
+        twitter_user_account.last_interaction_timestamp = clock.unix_timestamp;
         Ok(())
+    }
+    
+    pub fn get_number_of_tweets_by_user(ctx: Context<GetNumberOfTweetsByUser>) -> Result<u64>{
+        let twitter_user_account = &mut ctx.accounts.twitter_user_account;
+        Ok(twitter_user_account.tweet_count)
     }
     pub fn delete_twitter_account(_ctx: Context<DeleteTwitterAccount>) -> Result<()> {
         Ok(())
     }
     pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> Result<()> {
-        let tweet = &mut ctx.accounts.tweet_account;
-        let twitter_user_account = &mut ctx.accounts.twitter_user_account;
+        let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet_account;
+        let twitter_user_account: &mut Account<TwitterUser> = &mut ctx.accounts.twitter_user_account;
         let clock: Clock = Clock::get().unwrap();
         if topic.as_bytes().len() > 64 {
             return Err(ErrorCode::TopicTooLong.into())
@@ -38,14 +46,17 @@ pub mod on_chain_twitter {
         if content.as_bytes().len() > 512 {
             return Err(ErrorCode::ContentTooLong.into())
         }
-        twitter_user_account.tweet_count +=1;
         tweet.timestamp = clock.unix_timestamp;
         tweet.topic = topic;
         tweet.content = content;
         tweet.author = *ctx.accounts.author.key;
+        twitter_user_account.tweet_count +=1;
+        tweet.tweet_number = twitter_user_account.tweet_count;
+        twitter_user_account.last_interaction_timestamp = clock.unix_timestamp;
         Ok(())
     }
     pub fn update_tweet(ctx: Context<UpdateTweet>, topic: String, content: String) -> Result<()> {
+        let twitter_user_account:  &mut Account<TwitterUser> = &mut ctx.accounts.twitter_user_account;
         let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet_account;
         let clock: Clock = Clock::get().unwrap();
         if topic.as_bytes().len() > 64 {
@@ -57,6 +68,8 @@ pub mod on_chain_twitter {
         tweet.timestamp = clock.unix_timestamp;
         tweet.topic = topic;
         tweet.content = content;
+        tweet.tweet_number = twitter_user_account.tweet_count;
+        twitter_user_account.last_interaction_timestamp = clock.unix_timestamp;
         Ok(())
     }
     pub fn delete_tweet(_ctx: Context<DeleteTweet>) -> Result<()> {
@@ -67,38 +80,43 @@ pub mod on_chain_twitter {
 #[derive(Accounts)]
 pub struct CreateTwitterAccount <'info>{
     #[account(mut)]
-    pub authority: Signer<'info>,
-    #[account(init, payer = authority, space = TwitterUser::LEN, seeds = [b"twitter_user".as_ref(), authority.key().as_ref()], bump)]
+    pub author: Signer<'info>,
+    #[account(init, payer = author, space = TwitterUser::LEN, seeds = [b"twitter_user".as_ref(), author.key().as_ref()], bump)]
     pub twitter_user_account: Account<'info, TwitterUser>,
     pub system_program: Program<'info, System>,
 }
 #[derive(Accounts)]
 pub struct ChangeUserName <'info>{
-    #[account(mut, has_one = authority, seeds = [b"twitter_user".as_ref(),authority.key().as_ref()], bump = twitter_user_account.bump)]
+    #[account(mut, has_one = author, seeds = [b"twitter_user".as_ref(),author.key().as_ref()], bump = twitter_user_account.bump)]
     pub twitter_user_account: Account<'info, TwitterUser>,
-    pub authority: Signer<'info>,
+    pub author: Signer<'info>,
 }
 #[derive(Accounts)]
 pub struct DeleteTwitterAccount<'info>{
-    #[account(mut, has_one = authority, seeds = [b"twitter_user".as_ref(), authority.key().as_ref()], bump = twitter_user_account.bump, close = authority)]
+    #[account(mut, has_one = author, seeds = [b"twitter_user".as_ref(), author.key().as_ref()], bump = twitter_user_account.bump, close = author)]
     pub twitter_user_account: Account<'info, TwitterUser>,
-    pub authority: Signer<'info>,
+    pub author: Signer<'info>,
 }
-
+#[derive(Accounts)]
+pub struct GetNumberOfTweetsByUser<'info>{
+    #[account(mut, seeds = [b"twitter_user".as_ref(), author.key().as_ref()], bump = twitter_user_account.bump)]
+    pub twitter_user_account: Account<'info, TwitterUser>,
+    pub author: UncheckedAccount<'info>,
+}
 #[derive(Accounts)]
 pub struct SendTweet <'info>{
-    #[account(mut)]
-    pub author: Signer<'info>,
     #[account(mut,seeds = [b"twitter_user".as_ref(), author.key().as_ref()], bump = twitter_user_account.bump)]
     pub twitter_user_account: Account<'info, TwitterUser>,
     #[account(init, payer = author, space = Tweet::LEN,seeds = [b"tweet_account".as_ref(), author.key().as_ref(),&twitter_user_account.tweet_count.to_le_bytes()], bump)]
     pub tweet_account: Account<'info, Tweet>,
+    #[account(mut)]
+    pub author: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
 pub struct UpdateTweet <'info>{
-    #[account(seeds = [b"twitter_user".as_ref(), author.key().as_ref()], bump = twitter_user_account.bump)]
+    #[account(mut, has_one = author, seeds = [b"twitter_user".as_ref(), author.key().as_ref()], bump = twitter_user_account.bump)]
     pub twitter_user_account: Account<'info, TwitterUser>,
     #[account(mut,has_one = author, seeds = [b"tweet_account".as_ref(), author.key().as_ref(),&twitter_user_account.tweet_count.to_le_bytes()], bump = twitter_user_account.bump)]
     pub tweet_account: Account<'info, Tweet>,
@@ -107,7 +125,7 @@ pub struct UpdateTweet <'info>{
 
 #[derive(Accounts)]
 pub struct DeleteTweet <'info>{
-    #[account(seeds = [b"twitter_user".as_ref(), author.key().as_ref()], bump = twitter_user_account.bump)]
+    #[account(mut, has_one = author, seeds = [b"twitter_user".as_ref(), author.key().as_ref()], bump = twitter_user_account.bump)]
     pub twitter_user_account: Account<'info, TwitterUser>,
     #[account(mut, has_one = author, seeds = [b"tweet_account".as_ref(), author.key().as_ref(),&twitter_user_account.tweet_count.to_le_bytes()], bump= twitter_user_account.bump, close = author)]
     pub tweet_account: Account<'info, Tweet>,
@@ -141,8 +159,9 @@ impl Tweet{
 #[account]
 pub struct TwitterUser{
     pub tweet_count: u64,
-    pub authority: Pubkey,
-    pub username: String,    
+    pub author: Pubkey,
+    pub username: String,
+    pub last_interaction_timestamp: i64,    
     pub bump: u8,
 }
 const MAX_TWEET_COUNT_SIZE: usize = 8;
@@ -154,7 +173,8 @@ impl TwitterUser{
                 + MAX_TWEET_COUNT_SIZE
                 + MAX_AUTHORITY_LENGTH
                 + STRING_LENGTH_PREFIX + MAX_USERNAME_LENGTH
-                +  BUMP_LENGTH;
+                + MAX_TIMESTAMP_SIZE
+                + BUMP_LENGTH;
 }
 #[error_code]
 pub enum ErrorCode {
